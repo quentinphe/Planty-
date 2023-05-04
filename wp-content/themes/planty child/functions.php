d0104be648caca626653647612fbce335b525422
quentinphe <prudhommeq23@gmail.com>
2023-05-04 17:48:57 +0200

    Form command and header customizer finish

--- wp-content/themes/planty child/functions.php
+++ wp-content/themes/planty child/functions.php
@@ -7,6 +7,84 @@ function theme_enqueue_styles() {
 }
 /* AFFICHER LE HEADER */
 
-register_nav_menus( array(
-	'primary' => __( 'Primary Menu', 'Planty-child' ),
-) );
+add_action( 'wp_enqueue_scripts', 'planty_enqueue_styles' );
+function planty_enqueue_styles() {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+
+// Fonction de configuration du thème enfant
+add_action( 'after_setup_theme', 'planty_child_theme_setup' );
+
+function planty_child_theme_setup() {
+	// Définition de la section pour le header dans le customizer
+	add_action( 'customize_register', 'planty_customizer_sections' );
+	function planty_customizer_sections( $wp_customize ) {
+		$wp_customize->add_section( 'planty_header', array(
+			'title' => __( 'Header', 'Planty-child' ),
+			'description' => __( 'Customize the header of your site', 'Planty-child' ),
+			'priority' => 1
+		) );
+
+		// Définition des options de personnalisation pour le header
+		$wp_customize->add_setting( 'header_background_color', array(
+			'default' => '#ffffff',
+			'sanitize_callback' => 'sanitize_hex_color',
+			'transport' => 'postMessage'
+		) );
+
+		$wp_customize->add_setting( 'header_logo', array(
+			'default' => '',
+			'sanitize_callback' => 'esc_url_raw'
+		) );
+
+		$wp_customize->add_setting( 'header_site_title', array(
+			'default' => '',
+			'sanitize_callback' => 'sanitize_text_field'
+		) );
+
+		$wp_customize->add_setting( 'header_font_family', array(
+			'default' => 'Arial, sans-serif',
+			'sanitize_callback' => 'sanitize_text_field'
+		) );
+
+		$wp_customize->add_setting( 'header_text_color', array(
+			'default' => '#000000',
+			'sanitize_callback' => 'sanitize_hex_color',
+			'transport' => 'postMessage'
+		) );
+
+		$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'header_background_color', array(
+			'label' => __( 'Header Background Color', 'Planty-child' ),
+			'section' => 'planty_header',
+			'settings' => 'header_background_color'
+		) ) );
+
+		$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'header_logo', array(
+			'label' => __( 'Header Logo', 'Planty-child' ),
+			'section' => 'planty_header',
+			'settings' => 'header_logo'
+		) ) );
+
+		$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'header_site_title', array(
+			'label' => __( 'Header Site Title', 'Planty-child' ),
+			'section' => 'planty_header',
+			'settings' => 'header_site_title',
+			'type' => 'text'
+		) ) );
+
+		$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'header_font_family', array(
+			'label' => __( 'Header Font Family', 'Planty-child' ),
+			'section' => 'planty_header',
+			'settings' => 'header_font_family',
+			'type' => 'text'
+		) ) );
+
+		$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'header_text_color', array(
+			'label' => __( 'Header Text Color', 'Planty-child' ),
+			'section' => 'planty_header',
+			'settings' => 'header_text_color'
+		) ) );
+
+        
+    }
+}
--- wp-content/themes/planty child/header.php
+++ wp-content/themes/planty child/header.php
@@ -8,18 +8,37 @@
 </head>
 
 <body <?php body_class(); ?>>
-	<header>
+	<?php
+		$header_bg_color = get_theme_mod( 'header_background_color', '#ffffff' );
+		$header_logo = get_theme_mod( 'header_logo' );
+		$header_title = get_theme_mod( 'header_title', get_bloginfo( 'name' ) );
+		$header_font = get_theme_mod( 'header_font', 'Arial' );
+		$header_text_color = get_theme_mod( 'header_text_color', '#000000' );
+	?>
+	<header style="background-color: <?php echo esc_attr( $header_bg_color ); ?>">
 		<div class="header-container">
 			<div class="logo">
-				<a href="http://localhost:8888/Planty/?preview_id=6&preview_nonce=caac230ba8&preview=true">
-					<img class="logo-header" src="http://localhost:8888/Planty/wp-content/uploads/2023/04/cropped-Logo.png" alt="logo">
+				<a href="<?php echo esc_url( home_url() ); ?>">
+					<?php if ( $header_logo ) : ?>
+						<img class="logo-header" src="<?php echo esc_url( $header_logo ); ?>" alt="<?php echo esc_attr( $header_title ); ?>">
+					<?php else : ?>
+						<h1 class="site-title" style="font-family: <?php echo esc_attr( $header_font ); ?>; color: <?php echo esc_attr( $header_text_color ); ?>;"><?php echo esc_html( $header_title ); ?></h1>
+					<?php endif; ?>
 				</a>
 			</div>
 			<nav>
-				<ul>
-					<li><a href="http://localhost:8888/Planty/nous-rencontrer/?preview_id=10&preview_nonce=1f2157ea77&preview=true">Nous rencontrer</a></li>
-					<li class="command-red"><a href="http://localhost:8888/Planty/commander-2/?preview=true">Commander</a></li>
-				</ul>
+				<?php
+					wp_nav_menu( array(
+						'theme_location' => 'primary',
+						'container' => false,
+						'menu_id' => 'header-menu'
+					) );
+				?>
 			</nav>
+			<div class="header-widgets">
+				<?php if ( is_active_sidebar( 'header_widget_area' ) ) : ?>
+					<?php dynamic_sidebar( 'header_widget_area' ); ?>
+				<?php endif; ?>
+			</div>
 		</div>
 	</header>
