e63fb055bdc445cde13dbccb73a1b709e6c2e9dd
quentinphe <prudhommeq23@gmail.com>
2023-05-05 16:48:10 +0200

    finition presque terminer

--- wp-content/themes/planty child/functions.php
+++ wp-content/themes/planty child/functions.php
@@ -12,11 +12,14 @@ function planty_enqueue_styles() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 
-// Fonction de configuration du thème enfant
+/* FONCTION DE CONFIGURATION DU THÈME ENFANT */
+
 add_action( 'after_setup_theme', 'planty_child_theme_setup' );
 
 function planty_child_theme_setup() {
-	// Définition de la section pour le header dans le customizer
+
+	/* DÉFINITION DE CUSTOMISATION POUR LE CUSTOMIZER ET LE HEADER */
+
 	add_action( 'customize_register', 'planty_customizer_sections' );
 	function planty_customizer_sections( $wp_customize ) {
 		$wp_customize->add_section( 'planty_header', array(
@@ -25,30 +28,11 @@ function planty_child_theme_setup() {
 			'priority' => 1
 		) );
 
-		// Définition des options de personnalisation pour le header
+		/* DÉFINITION DE PERSONNALISATION POUR LE HEADER */
+		/* OPTION BACKGROUND COLOR HEADER */
+
 		$wp_customize->add_setting( 'header_background_color', array(
 			'default' => '#ffffff',
-			'sanitize_callback' => 'sanitize_hex_color',
-			'transport' => 'postMessage'
-		) );
-
-		$wp_customize->add_setting( 'header_logo', array(
-			'default' => '',
-			'sanitize_callback' => 'esc_url_raw'
-		) );
-
-		$wp_customize->add_setting( 'header_site_title', array(
-			'default' => '',
-			'sanitize_callback' => 'sanitize_text_field'
-		) );
-
-		$wp_customize->add_setting( 'header_font_family', array(
-			'default' => 'Arial, sans-serif',
-			'sanitize_callback' => 'sanitize_text_field'
-		) );
-
-		$wp_customize->add_setting( 'header_text_color', array(
-			'default' => '#000000',
 			'sanitize_callback' => 'sanitize_hex_color',
 			'transport' => 'postMessage'
 		) );
@@ -59,11 +43,25 @@ function planty_child_theme_setup() {
 			'settings' => 'header_background_color'
 		) ) );
 
+		/* OPTION LOGO HEADER */
+
+		$wp_customize->add_setting( 'header_logo', array(
+			'default' => '',
+			'sanitize_callback' => 'esc_url_raw'
+		) );
+
 		$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'header_logo', array(
 			'label' => __( 'Header Logo', 'Planty-child' ),
 			'section' => 'planty_header',
 			'settings' => 'header_logo'
 		) ) );
+
+		/* OPTION SITE TITLE */
+
+		$wp_customize->add_setting( 'header_site_title', array(
+			'default' => '',
+			'sanitize_callback' => 'sanitize_text_field'
+		) );
 
 		$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'header_site_title', array(
 			'label' => __( 'Header Site Title', 'Planty-child' ),
@@ -72,6 +70,13 @@ function planty_child_theme_setup() {
 			'type' => 'text'
 		) ) );
 
+		/* OPTION FONT FAMILY HEADER */
+
+		$wp_customize->add_setting( 'header_font_family', array(
+			'default' => 'Arial, sans-serif',
+			'sanitize_callback' => 'sanitize_text_field'
+		) );
+
 		$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'header_font_family', array(
 			'label' => __( 'Header Font Family', 'Planty-child' ),
 			'section' => 'planty_header',
@@ -79,12 +84,49 @@ function planty_child_theme_setup() {
 			'type' => 'text'
 		) ) );
 
+		/* OPTION COLOR TEXTE HEADER */
+
+		$wp_customize->add_setting( 'header_text_color', array(
+			'default' => '#000000',
+			'sanitize_callback' => 'sanitize_hex_color',
+			'transport' => 'postMessage'
+		) );
+
 		$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'header_text_color', array(
 			'label' => __( 'Header Text Color', 'Planty-child' ),
 			'section' => 'planty_header',
 			'settings' => 'header_text_color'
 		) ) );
 
-        
+		/* OPTION TAILLE LOGO */
+
+		$wp_customize->add_setting('logo_size', array(
+			'default' => '100%',
+			'transport' => 'refresh',
+		));
+
+		$wp_customize->add_control('logo_size_control', array(
+			'label' => __('Taille du logo', 'textdomain'),
+			'section' => 'logo_options',
+			'settings' => 'logo_size',
+			'type' => 'text',
+			'input_attrs' => array(
+				'placeholder' => __('Ex: 100%, 200px, etc.', 'textdomain'),
+			),
+		));  
     }
 }
+
+
+/* FONCTION $HOOKS COMPTE ADMIN */
+
+function mon_menu_admin($items, $args) {
+    if (is_user_logged_in() && current_user_can('manage_options')) {
+        $admin_link = '<li><a href="' . admin_url() . '">' . __('Admin', 'text-domain') . '</a></li>';
+        $items_array = explode('</li>', $items);
+        array_splice($items_array, 1, 0, $admin_link);
+        $items = implode('</li>', $items_array);
+    }
+    return $items;
+}
+add_filter('wp_nav_menu_items', 'mon_menu_admin', 10, 2);
--- wp-content/themes/planty child/header.php
+++ wp-content/themes/planty child/header.php
@@ -14,31 +14,33 @@
 		$header_title = get_theme_mod( 'header_title', get_bloginfo( 'name' ) );
 		$header_font = get_theme_mod( 'header_font', 'Arial' );
 		$header_text_color = get_theme_mod( 'header_text_color', '#000000' );
+		$logo_size = get_theme_mod('logo_size', '100%');
+
 	?>
 	<header style="background-color: <?php echo esc_attr( $header_bg_color ); ?>">
 		<div class="header-container">
 			<div class="logo">
-				<a href="<?php echo esc_url( home_url() ); ?>">
-					<?php if ( $header_logo ) : ?>
-						<img class="logo-header" src="<?php echo esc_url( $header_logo ); ?>" alt="<?php echo esc_attr( $header_title ); ?>">
-					<?php else : ?>
-						<h1 class="site-title" style="font-family: <?php echo esc_attr( $header_font ); ?>; color: <?php echo esc_attr( $header_text_color ); ?>;"><?php echo esc_html( $header_title ); ?></h1>
-					<?php endif; ?>
-				</a>
-			</div>
-			<nav>
-				<?php
-					wp_nav_menu( array(
-						'theme_location' => 'primary',
-						'container' => false,
-						'menu_id' => 'header-menu'
-					) );
-				?>
-			</nav>
-			<div class="header-widgets">
-				<?php if ( is_active_sidebar( 'header_widget_area' ) ) : ?>
-					<?php dynamic_sidebar( 'header_widget_area' ); ?>
+			<a href="<?php echo esc_url( home_url() ); ?>" class="site-logo" style="font-size: <?php echo esc_attr($logo_size); ?>;">
+				<?php if ( $header_logo ) : ?>
+					<img class="logo-header" src="<?php echo esc_url( $header_logo ); ?>" alt="<?php echo esc_attr( $header_title ); ?>">
+				<?php else : ?>
+					<h1 class="site-title" style="font-family: <?php echo esc_attr( $header_font ); ?>; color: <?php echo esc_attr( $header_text_color ); ?>;"><?php echo esc_html( $header_title ); ?></h1>
 				<?php endif; ?>
-			</div>
+			</a>
 		</div>
-	</header>
+		<nav>
+			<?php
+				wp_nav_menu( array(
+					'theme_location' => 'primary',
+					'container' => false,
+					'menu_id' => 'header-menu'
+				) );
+			?>
+		</nav>
+		<div class="header-widgets">
+			<?php if ( is_active_sidebar( 'header_widget_area' ) ) : ?>
+				<?php dynamic_sidebar( 'header_widget_area' ); ?>
+			<?php endif; ?>
+		</div>
+	</div>
+</header>
